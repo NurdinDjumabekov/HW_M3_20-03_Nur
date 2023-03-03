7e709ce4809c02a237c646df1959256a6174e12f
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-03-03 22:22:10 +0600

    Add files via upload

--- DZ7_Nurdin_20-03/app.js
+++ DZ7_Nurdin_20-03/app.js
@@ -1,42 +1,36 @@
-const block = document.querySelector(".block");
-const btnPrev = document.querySelector(".btn-prev");
-const btnNext = document.querySelector(".btn-next");
-let count = 1;
+const main_info_inner = document.querySelector(".main_info_inner");
 
-const goToFetch = () => {
-  fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-    .then((response) => response.json())
-    .then((data) => {
-      block.innerHTML = `
-              <h2>${data.title}</h2>
-              <span>${data.id}</span>
-              <h3>${data.completed}</h3>
-          `;
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const fetchRequest = async () => {
+  try {
+    const rest = await fetch(url);
+    const info = await rest.json();
+    info.map((item) => {
+      console.log(item.title);
+      const newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "newDiv");
+      main_info_inner.append(newDiv);
+      const h3 = document.createElement("h3");
+      h3.innerText = item.title;
+      newDiv.append(h3);
+      const newDivInner = document.createElement("div");
+      newDivInner.setAttribute("class", "newDivInner");
+      newDiv.append(newDivInner);
+      const p = document.createElement("p");
+      const img = document.createElement("img");
+      newDivInner.append(img);
+      img.setAttribute("class", "img_info");
+      newDivInner.append(p);
+      p.setAttribute("class", "p_text");
+      p.innerHTML = item.body;
+      img.src =
+        "https://cdnn1.img.sputnik-abkhazia.info/img/102951/73/1029517318_112:0:1889:1333_1920x0_80_0_0_479b92c01ccee383a0be7e65b46c1962.jpg";
     });
-};
-
-btnNext.addEventListener("click", () => {
-  count++;
-  goToFetch();
-});
-
-btnPrev.addEventListener("click", () => {
-  if (count > 1) {
-    count--;
-    goToFetch();
-  } else {
-    alert("error");
+  } catch {
+    console.error("error");
+  } finally {
+    console.warn("done!!!!!");
   }
-});
-
-/////////////////////////////////////////////////////////
-
-const btn_start = document.querySelector(".btn_start");
-
-btn_start.addEventListener("click", () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-});
+};
+fetchRequest();
